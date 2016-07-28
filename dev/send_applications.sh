0e46c6a82dfdd4d639889917db511a14d5a05267
pokitoz <florian.depraz@epfl.ch>
2016-07-28 16:31:07 +0200

    Modification to send includes folder

--- dev/send_applications.sh
+++ dev/send_applications.sh
@@ -43,8 +43,10 @@ pushd $applications_dir_r
 	echo -e "$c_info Sending sobel_filter_app/ folder to $sshcommand:~/Desktop $c_default"
 	sshpass -p "$env_sshpassword" scp -r sobel_filter_app/ $sshcommand:~/Desktop
 #	rsync -av --exclude '*.svn' user@server:/my/dir .
+	echo -e "$c_info Sending includes/ folder to $sshcommand:~/Desktop $c_default"
+	sshpass -p "$env_sshpassword" scp -r includes/ $sshcommand:~/Desktop
 
-
+	echo -e "$c_info Sending scripts to $sshcommand:~/Desktop $c_default"
 	sshpass -p "$env_sshpassword" scp -r *.sh $sshcommand:~/Desktop
 
 
